92367544b14aacd9776b14985779cbc995ab69c1
彭章锟 <532767597@qq.com>
2017-04-21 10:49:01 +0800

    Initial Commit

---
+++ UseProtobufDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseProtobufDemo
+//
+//  Created by pzk on 17/4/21.
+//  Copyright © 2017年 Aone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseProtobufDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UseProtobufDemo
+//
+//  Created by pzk on 17/4/21.
+//  Copyright © 2017年 Aone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
